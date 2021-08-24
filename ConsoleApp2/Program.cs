6f9e1c82c55a678c3814e71764e9c605d7b15c18
Jhenny <jhennyfferemmily@hotmail.com>
2021-08-23 23:21:11 -0300

    second commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var lunch = "lunch";
+            var starts = 12;
+            var ends = 13;
+
+            //My lunch starts at 12 and it ends at 13.
+            Console.WriteLine( $" My {lunch} starts at {starts} and it ends at {ends}");
+        }
+    }
+}
